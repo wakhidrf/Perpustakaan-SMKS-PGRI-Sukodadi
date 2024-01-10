891115884b0dde93b4d1b437ea89a1319aaa6443
wakhidrahmadfaisal@gmail.com <wakhidrahmadfaisal@gmail.com>
2024-01-11 06:06:00 +0700

    Update

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,4 @@
+<div class="py-6 px-6 text-center">
+  <p class="mb-0 fs-4">&copy; 2023, made with ❤️ by <a href="https://ikhsan3adi.github.io/" target="_blank" class="pe-1 text-primary text-decoration-underline">Ikhsan Satriadi</a></p>
+  <p class="mb-0 fs-4">&copy; 2024, maintained with ❤️ by <a href="https://linkedin.com/in/wakhidrf" target="_blank" class="pe-1 text-primary text-decoration-underline">Wakhid Rahmad Faisol</a></p>
+</div>
